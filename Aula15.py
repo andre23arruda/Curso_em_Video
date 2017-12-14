3cfe23389f34fdd116e6b1808daaf6664f5e1e05
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-14 16:29:29 -0200

    Update Aula15.py

--- Aula15.py
+++ Aula15.py
@@ -1,1 +1,107 @@
-#%% ----------------- DESAFIO
+#%% ----------------- DESAFIO 66 ------------------------
+soma = 0
+cont = 0
+while True:
+  novo =  int(input('Digite um numero (999 para parar): '))
+  if novo == 999:
+    break
+  soma += novo
+  cont += 1   
+print(f'A soma dos {cont} valores é {soma}')
+
+
+#%% ----------------- DESAFIO 67 ------------------------
+numero = 0
+while True:
+  numero = int(input('Deseja ver a tabuada de qual numero?'))
+  print('----------------------------------')
+  if numero < 0:
+    break
+  for i in range(0,11):
+    print(f'{numero} x {i} = {numero*i}')
+  print('----------------------------------')
+print('Encerrado')
+
+
+#%% ----------------- DESAFIO 68 ------------------------
+from random import *
+print('------------- PAR OU IMPAR -----------')
+n_vitorias = 0
+while True:
+  pc = randint(0,10)
+  escolha = input('Par ou impar [P/I]?')
+  jogador = int(input('Entre com um numero: '))
+  soma = pc+jogador
+  print(f'Voce jogou {jogador} e o computador {pc}.\nTotal = {soma}')
+  if soma%2 == 0:
+    jogo = 'P'
+  else:
+    jogo = 'I'
+  if escolha == jogo:
+    n_vitorias += 1
+    print(f'Voce VENCEU')
+    print('JOGAR NOVAMENTE...')
+  else:
+    print(f'Voce PERDEU')
+    break
+print(f'Voce ganhou {n_vitorias} vezes')
+
+
+#%% ----------------- DESAFIO 69 ------------------------
+mulheres20 = 0
+homens = 0
+adultos = 0
+escolha  = '0'
+
+while True:
+  sexo = '0'
+  escolha  = '0'
+  print('---------------- CADASTRO -------------------')
+  while sexo != M or sexo != F:
+    sexo = input('Digite o sexo da pessoa [M/F] ).upper()
+  idade = 'a'
+  while not(idade.isnumeric()):
+    idade = input('Digite a idade da pessoa: ')
+  idade = int(idade)
+  if idade > 18:
+    adultos += 1
+  if sexo == 'M':
+    homens += 1
+  if sexo == 'F' and idade < 20:
+    mulheres20 += 1
+  while escolha != 'S' or escolha != N:
+    escolha = input('Deseja continuar? [S/N]).upper
+  if escolha == 'N':
+    breake
+                    
+print(f'Adultos: {adultos}')
+print(f'Homens: {homens}')
+print(f'Mulheres abaixo dos 20: {mulheres20}')                    
+   
+     
+#%% ----------------- DESAFIO 70 ------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
